c7d87edd753e5035965107b98c3398daf8ef0f8c
Jonathan Gawrych <jonathan@goreact.com>
2019-10-20 16:52:17 -0600

    test: check sourcemap for correct coverage mapping

---
+++ e2e/__projects__/basic/components/Coverage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div />
+</template>
+
+<script>
+const coveredFn = () => {
+  const statement = true ? 'I' : 'E'
+}
+const uncoveredFn = () => {}
+
+const branch = true
+if (branch) {
+  coveredFn()
+} else {
+  uncoveredFn()
+}
+
+export default {
+  name: 'coverage'
+}
+</script>
--- e2e/__projects__/basic/test.js
+++ e2e/__projects__/basic/test.js
@@ -17,6 +17,10 @@ import Pug from './components/Pug.vue'
 import PugRelative from './components/PugRelativeExtends.vue'
 import Jsx from './components/Jsx.vue'
 import Constructor from './components/Constructor.vue'
+import istanbulSourceMapping from 'istanbul-lib-source-maps'
+import istanbulCoverage from 'istanbul-lib-coverage'
+import { transformSync } from '@babel/core'
+import babelPluginIstanbul from 'babel-plugin-istanbul'
 
 test('processes .vue files', () => {
   const wrapper = mount(Basic)
@@ -53,6 +57,148 @@ test('generates source maps using src attributes', () => {
   })
 
   expect(code).toMatchSnapshot()
+})
+
+test('generates source maps correct for mapping coverage', () => {
+  const filePath = resolve(__dirname, './components/Coverage.vue')
+  const fileString = readFileSync(filePath, { encoding: 'utf8' })
+
+  const mapStore = istanbulSourceMapping.createSourceMapStore()
+  const { code, map } = jestVue.process(fileString, filePath, {
+    moduleFileExtensions: ['js', 'vue']
+  })
+  mapStore.registerMap(filePath, map.toJSON())
+
+  const coverageMap = istanbulCoverage.createCoverageMap()
+  transformSync(code, {
+    filename: filePath,
+    plugins: [
+      [
+        babelPluginIstanbul,
+        {
+          onCover: (file, coverage) => {
+            coverageMap.addFileCoverage(coverage)
+          }
+        }
+      ]
+    ]
+  })
+
+  const mappedCoverage = mapStore
+    .transformCoverage(coverageMap)
+    .map.fileCoverageFor(filePath)
+
+  // const coveredFn = () => { /*...*/ }
+  expect(Object.values(mappedCoverage.fnMap)).toContainEqual(
+    expect.objectContaining({
+      name: 'coveredFn',
+      decl: {
+        start: { line: 6, column: 6 },
+        end: { line: 6, column: 15 }
+      }
+    })
+  )
+
+  // const uncoveredFn = () => {}
+  expect(Object.values(mappedCoverage.fnMap)).toContainEqual(
+    expect.objectContaining({
+      name: 'uncoveredFn',
+      decl: {
+        start: { line: 9, column: 6 },
+        end: { line: 9, column: 17 }
+      }
+    })
+  )
+
+  // if (branch) { /*...*/ } else { /*...*/ }
+  expect(Object.values(mappedCoverage.branchMap)).toContainEqual(
+    expect.objectContaining({
+      type: 'if',
+      locations: [
+        {
+          start: { line: 12, column: 0 },
+          end: { line: 16, column: Infinity }
+        },
+        {
+          start: { line: 12, column: 0 },
+          end: { line: 16, column: Infinity }
+        }
+      ]
+    })
+  )
+
+  // const statement = true ? "I" : "E"
+  expect(Object.values(mappedCoverage.branchMap)).toContainEqual(
+    expect.objectContaining({
+      type: 'cond-expr',
+      locations: [
+        {
+          start: { line: 7, column: 27 },
+          end: { line: 7, column: 33 }
+        },
+        {
+          start: { line: 7, column: 33 },
+          end: { line: 7, column: Infinity }
+        }
+      ]
+    })
+  )
+
+  // = () => { /*...*/ }
+  expect(Object.values(mappedCoverage.statementMap)).toContainEqual(
+    expect.objectContaining({
+      start: { line: 6, column: 18 },
+      end: { line: 8, column: Infinity }
+    })
+  )
+
+  // = true ? "I" : "E"
+  expect(Object.values(mappedCoverage.statementMap)).toContainEqual(
+    expect.objectContaining({
+      start: { line: 7, column: 20 },
+      end: { line: 7, column: Infinity }
+    })
+  )
+
+  // = () => {}
+  expect(Object.values(mappedCoverage.statementMap)).toContainEqual(
+    expect.objectContaining({
+      start: { line: 9, column: 20 },
+      end: { line: 9, column: Infinity }
+    })
+  )
+
+  // = true
+  expect(Object.values(mappedCoverage.statementMap)).toContainEqual(
+    expect.objectContaining({
+      start: { line: 11, column: 15 },
+      end: { line: 11, column: Infinity }
+    })
+  )
+
+  // if (branch) { /*...*/ } else { /*...*/ }
+  expect(Object.values(mappedCoverage.statementMap)).toContainEqual(
+    expect.objectContaining({
+      start: { line: 12, column: 0 },
+      end: { line: 16, column: Infinity }
+    })
+  )
+
+  // coveredFn()
+  expect(Object.values(mappedCoverage.statementMap)).toContainEqual(
+    expect.objectContaining({
+      start: { line: 13, column: 2 },
+      end: { line: 13, column: Infinity }
+    })
+  )
+
+  // uncoveredFn()
+  expect(Object.values(mappedCoverage.statementMap)).toContainEqual(
+    expect.objectContaining({
+      start: { line: 15, column: 2 },
+      end: { line: 15, column: Infinity }
+    })
+  )
 })
 
 test('processes .vue file using jsx', () => {
